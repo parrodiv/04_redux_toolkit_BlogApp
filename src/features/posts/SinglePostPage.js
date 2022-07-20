09d9866c41ea39713fe0bb7246c936898d138757
Alessandro Parrilla <paggio96@gmail.com>
2022-07-20 21:50:07 +0200

    useParams in SinglePostPage, PostExerpt component
    -remember PostExerpt component is in the list of all posts, SinglePostPage is a single post in a different page

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -5,11 +5,15 @@ import PostAuthor from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
 
+import { useParams } from 'react-router-dom'
 
 const SinglePostPage = () => {
   // retrieve postId
+  const { postId } = useParams()
+  console.log(typeof postId); // string so down below I put a + sign to convert string to number
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  const post = useSelector(state => selectPostById(state, +postId))
+  
 
   if(!post) {
     return (
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -107,7 +107,7 @@ export const getPostsStatus = (state) => state.posts.status
 export const getPostsError = (state) => state.posts.error
 
 export const selectPostById = (state, postId) =>
-  state.posts.posts.find((post) => post.id === postId)
+   state.posts.posts.find(post => post.id === postId)
 
 export const { postAdded, reactionAdded } = postsSlice.actions
 
