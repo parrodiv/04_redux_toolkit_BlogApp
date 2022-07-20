cbe9982cbabadbac9b32bede2a5131369b390c1f
Alessandro Parrilla <paggio96@gmail.com>
2022-07-20 19:24:24 +0200

    selectPostById selector adn SinglePostPage component

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,35 @@
+import { useSelector } from 'react-redux'
+import { selectPostById } from './postsSlice'
+
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
+
+const SinglePostPage = () => {
+  // retrieve postId
+
+  const post = useSelector((state) => selectPostById(state, postId))
+
+  if(!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
+
+export default SinglePostPage
