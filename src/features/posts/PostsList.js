43cc6c7bfd7dd489f0e9580abf4833a1e617e8fd
Alessandro Parrilla <paggio96@gmail.com>
2022-07-21 21:58:34 +0200

    Loading API posts data at app load moving dispatch(fetchPosts()) in index.js

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,20 +1,13 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postsSlice";
-import { useEffect } from "react";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
-    const dispatch = useDispatch();
 
     const posts = useSelector(selectAllPosts);
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch])
 
     let content;
     if (postStatus === 'loading') {
